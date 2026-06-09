e08f2a80f2210497f84151efcfdd23cd3011706d
designed-by-sabina <sabinabdulmanova@gmail.com>
2026-06-09 20:44:53 +0300

    weather api key kine updaten.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -2,11 +2,10 @@ console.log("this works");
 const API_KEY = config.WEATHER_API_KEY
 
 function getWeatherData() {
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip={zip code}&APPID=${WEATHER_API_KEY}`)
+fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
  .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error('Error fetching data:', error));
   };
 
 getWeatherData()
-
