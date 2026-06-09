8cce2398e25d26cae182e7af00f850ff726a63e7
designed-by-sabina <sabinabdulmanova@gmail.com>
2026-06-09 20:09:57 +0300

    json.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -3,8 +3,14 @@ const API_KEY = config.WEATHER_API_KEY
 
 function getWeatherData() {
 fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${d59248058dfd6e856147f76f18c07b13}`)
-  .then(response => response.json())
-  .then(data => console.log(data));
+  .then((response) => { 
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data)});
+  .catch(function(error) {
+    console.log(error);
+  });
   }
 
 getWeatherData()
