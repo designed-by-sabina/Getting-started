bc0dc4c53415f8be06d34f1894e168ed0fa415f3
designed-by-sabina <sabinabdulmanova@gmail.com>
2026-06-09 20:14:36 +0300

    error logi update.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -3,14 +3,10 @@ const API_KEY = config.WEATHER_API_KEY
 
 function getWeatherData() {
 fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${d59248058dfd6e856147f76f18c07b13}`)
-  .then((response) => { 
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data)});
-  .catch(function(error) {
-    console.log(error);
-  });
-  }
+ .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error fetching data:', error));
+  };
 
 getWeatherData()
+
