2096724f10bd71332eaa7b9a5900250d810dac18
sabina-tochka <sabinabdulmanova@gmail.com>
2026-06-11 12:42:03 +0300

    Update main.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,13 +1,17 @@
-const API_KEY = config.WEATHER_API_KEY
-
 let input = document.querySelector(".zipcode");
 let btn = document.querySelector(".search-button");
-btn.addEventListener('click', getZipCode);
+let form = document.querySelector("form");
 
-getWeatherData = (zip) => {
-  let API_KEY = config.WEATHER_API_KEY;
-  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${ZIP_CODE}&APPID=${API_KEY}`;
+let CITY_NAME = document.querySelector(".city_name");
+let CITY_TEMP = document.querySelector(".temperature");
 
+
+// write a function to get weather data
+  const getWeatherData = (zip) => {
+  // store your open weather API Key
+  const API_KEY = config.WEATHER_API_KEY;
+  // store the API endpoint and API key
+  const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
 
 
 function getWeatherData(zip) {
@@ -16,10 +20,15 @@ fetch(API_ENDPOINT)
  .then(data => {
   // store the data in a variable of our choosing
   let local_weather_data = data;
-  // log the data to the browser console
-  console.log(local_weather_data);
+      // manipulate the city name content
+      CITY_NAME.textContent = local_weather_data.name;
+      // process the temperature data before manipulating the content
+      let weather_in_celsius = Math.round(
+        local_weather_data.main.temp - 273
+      );
+      // manipulate the temperature content
+      CITY_TEMP.textContent = weather_in_celsius + " C"
   });
-.catch(error => console.error('Error fetching data:', error));
 };    
 
 
@@ -30,3 +39,8 @@ const getZipCode = (e) => {
 };
 
 btn.addEventListener("click", getZipCode);
+
+
+
+
+  
